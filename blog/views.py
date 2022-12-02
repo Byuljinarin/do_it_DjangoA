6cfb5d9f85fbefc4cfa9df6534055513f7f10ccc
SeonWoong Yang <byuljinarin@gmail.com>
2022-12-02 17:15:57 +0900

    로그인 한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post, Category, Tag
 
 class PostList(ListView) :
     model = Post
     ordering = '-pk'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **fkwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
@@ -33,6 +34,21 @@ def category_page(request, slug):
         }
     )
 
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    )
+
 class PostDetail(DetailView) :
     model = Post
 
@@ -42,7 +58,17 @@ class PostDetail(DetailView) :
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
 
 
